aec99b0deabd6ea3be9e322fdb33d2741cd28b84
Morgen Peschke <morgen.peschke@gmail.com>
2019-05-23 14:15:19 -0700

    miller:5.3.0

---
+++ miller/rebuild.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+cd "$DIR"
+docker build -t miller-pending .
+TAG=$(docker run --rm -i miller-pending --version | cut -d' ' -f2)
+
+docker tag miller-pending morgenpeschke/miller:"$TAG"
+docker tag miller-pending morgenpeschke/miller:latest
+docker rmi miller-pending
