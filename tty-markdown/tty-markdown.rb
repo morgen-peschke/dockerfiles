6b39dc037bd19b66f169dff92b1e17e9c2c974f4
Morgen Peschke <morgen.peschke@gmail.com>
2020-06-12 14:45:21 -0700

    tty-markdown: set column width from env

--- tty-markdown/tty-markdown.rb
+++ tty-markdown/tty-markdown.rb
@@ -1,2 +1,4 @@
 require 'tty-markdown'
-puts TTY::Markdown.parse STDIN.read
+width = ENV['COLUMNS'].to_i
+width = 80 unless width.to_s == ENV['COLUMNS']
+puts TTY::Markdown.parse(STDIN.read, width: width)
