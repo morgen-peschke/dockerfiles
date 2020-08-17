20c3905085bacfc3cacd5b5bfd94893264724687
Morgen Peschke <morgen.peschke@gmail.com>
2020-08-17 12:10:15 -0700

    Bump miller version to 5.8.0

--- miller/rebuild.bash
+++ miller/rebuild.bash
@@ -4,7 +4,7 @@ DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 cd "$DIR"
 docker build -t miller-pending .
-TAG=$(docker run --rm -i miller-pending --version | cut -d' ' -f2)
+TAG=$(docker run --rm -i miller-pending --version | cut -d'v' -f2)
 
 docker tag miller-pending morgenpeschke/miller:"$TAG"
 docker tag miller-pending morgenpeschke/miller:latest
